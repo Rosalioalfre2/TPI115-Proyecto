63e6910b7dfb7d211ccc01d608cb1ab877730b25
abenabi <89744028+abenabi@users.noreply.github.com>
2021-12-07 11:10:33 -0600

    javascript

---
+++ server/videos.js
@@ -0,0 +1,21 @@
+mostrarVideos();
+
+function obtenerVideos(){
+    var videos
+    fetch('https://retoolapi.dev/HZUivn/videos')
+    .then(res => res.json())
+    .then(data =>{
+        videos =data;
+    })
+    return videos;
+}
+
+function mostrarVideos(){
+    var videos=obtenerVideos();
+    var contenedor=document.getElementById("videos");
+    var cantidadVideos=videos.length;
+    for(i=0;i<cantidadVideos;i++){
+        contenedor.innerHTML='<article><h2>'+videos[i].titulo+'</h2><iframe class="direccion" width="720" height="405" src='+videos[i].direccion+'title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></article>';
+    }
+    console.log(videos[0].titulo);
+}
